6bdf436ed1bf7d11a107a0635dcaa9c2fe77d1dd
Kyle Richelhoff <kyle@mythcomputers.ca>
2015-02-27 11:38:03 -0700

    ->Basics of a login system working

--- framework/views.py
+++ framework/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login as django_login
+from django.views.decorators.csrf import csrf_protect
+
 # Create your views here.
 
 
@@ -7,8 +10,16 @@ def login(request):
     """If user is logged in, redirect to dashboard, else, render login template."""
     if request.user.is_authenticated():
         return redirect(dashboard)
-    else:
-        return render(request, 'framework/login.html')
+    elif request.method == "POST":
+        username = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # Password is good
+            if user.is_active:
+                django_login(request, user)
+                return redirect(dashboard)
+    return render(request, 'framework/login.html', {'body_class': 'login-page'})
 
 
 @login_required
