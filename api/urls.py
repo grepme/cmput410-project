a2a9ea6894fc1c0f5b8c281e64f6634c4be763f3
varsis <chris@varsisstudio.com>
2015-03-08 16:35:12 -0600

    added paging to urls

--- api/urls.py
+++ api/urls.py
@@ -2,20 +2,20 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('api.views',
                         # get posts that are visible to current authenticated user
-                       (r'^author/posts$', 'get_posts'),
+                       (r'^author/posts(?:/(?P<page>\d+))?/$', 'get_posts'),
 
                         # get posts by a specific author that the current authenticated user
                         # can view
-                       (r'^author/(?P<author_id>[^/]+)/posts$', 'get_posts'),
+                       (r'^author/(?P<author_id>[^/]+)/posts(?:/(?P<page>\d+))?/$', 'get_posts'),
 
                        # Get a specific post
-                       (r'^posts/(?P<post_id>[^/]+)$', 'get_posts'),
+                       (r'^posts/(?P<post_id>[^/]+)/$', 'get_post'),
 
                        #See if a author_id is a friend with author_2_id
-                       (r'^friends/(?P<author_id>[^/]+)/(?P<author_2_id>[^/]+)$', 'get_posts'),
+                       (r'^friends/(?P<author_id>[^/]+)/(?P<author_2_id>[^/]+)$', 'is_friend'),
 
                        # POST authors, returns list of friends in the list
-                       (r'^friends/(?P<author_id>[^/]+)$', ''),
+                       (r'^friends/(?P<author_id>[^/]+)$', 'get_friends'),
 
                        # Make a friend request with another user
                        (r'^friendrequest$', 'friend_request'),
