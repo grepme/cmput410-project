9bea4ec473d41ca1beaff3e0027d70cb96348fb4
Airplay <kyle@mythcomputers.ca>
2015-03-08 16:32:50 -0600

    Messed up....

--- dispatch_files/urls.py
+++ dispatch_files/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('dispatch_files.views',
-                       (r'images/(\d{1,4})/(\d{1,2})/(\d{1,2})/(\w+).JPG$', 'images', {'type': "JPG"}),
-                       (r'images/(\d{1,4})/(\d{1,2})/(\d{1,2})/(\w+).PNG$', 'images', {'type': "PNG"}),
-                       (r'images/(\d{1,4})/(\d{1,2})/(\d{1,2})/(\w+).GIF$', 'images', {'type': "GIF"})
+                       (r'images/(\d{1,4})/(\d{1,2})/(\d{1,2})/(\w+).JPG$', 'images', {'image_type': "JPG"}),
+                       (r'images/(\d{1,4})/(\d{1,2})/(\d{1,2})/(\w+).PNG$', 'images', {'image_type': "PNG"}),
+                       (r'images/(\d{1,4})/(\d{1,2})/(\d{1,2})/(\w+).GIF$', 'images', {'image_type': "GIF"})
 
 )
