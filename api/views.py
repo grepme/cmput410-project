9ccb9e4eb08b2955c13c8f12a0940f2f49edb222
varsis <chris@varsisstudio.com>
2015-03-10 14:14:59 -0600

    decorator for accept types

--- api/views.py
+++ api/views.py
@@ -6,8 +6,9 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from django.http import HttpResponse
 from django.core import serializers
+import logging
 import json
-import pickle
+
 
 from comments.models import Comment
 from friends.models import Friend, Follow
@@ -16,6 +17,46 @@ from tags.models import Tag
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+
+
+#TODO Move to another file...
+from functools import wraps
+from django.utils.decorators import available_attrs
+logger = logging.getLogger('django.request')
+
+def require_http_accept(request_accept_list):
+    """
+    Decorator to make a view only accept particular requests with Accept types.  Usage:
+
+        @require_http_accept(["application/json", "text/html"])
+        def my_view(request):
+            # I can assume now that only requests with accepts within the list make it this far
+            # ...
+
+    """
+    def decorator(func):
+        @wraps(func, assigned=available_attrs(func))
+        def inner(request, *args, **kwargs):
+
+            list_extra = list(request_accept_list)
+            list_extra.append('*/*')
+
+            accept = request.META.get('Accept')
+
+            # none condition we assume */*
+            if accept not in request_accept_list and accept is not None:
+                logger.warning('Accept Not Allowed (%s): %s', request.META.get('Accept'), request.path,
+                    extra={
+                        'status_code': 406,
+                        'request': request
+                    }
+                )
+                response = HttpResponse("Accept Not Allowed (%s): %s", request.META.get('Accept'), request.path)
+                response.status_code = 406
+                return response
+            return func(request, *args, **kwargs)
+        return inner
+    return decorator
 
 # Create your views here.
 
@@ -35,63 +76,56 @@ class SetEncoder(json.JSONEncoder):
 
 @login_required
 @require_http_methods(["GET"])
+@require_http_accept(['application/json'])
 def get_posts(request,author_id=None,page="0"):
     #author/posts
     #author/author_id/posts
 
-    #assume */*
-    valid_accept = True
+    query = ( Q(visibility=Post.private, author__username=request.user.username) |
+        Q(visibility=Post.public) | Q(visibility=Post.server) |
+        Q(visibility=Post.friend, author__accepter=request.user.id) |
+        Q(visibility=Post.friend, author__requester=request.user.id) |
+        Q(visibility=Post.FOAF, author__requester__requester=request.user.id) |
+        Q(visibility=Post.FOAF, author__requester__accepter=request.user.id) |
+        Q(visibility=Post.FOAF, author__accepter__requester=request.user.id) |
+        Q(visibility=Post.FOAF, author__accepter__accepter=request.user.id) )
 
-    # check specificed content
-    if 'Accept' in request.META:
-        accept_type = request.META['Accept']
-        valid_accept = check_accept_type(accept_type)
+    # user specified a specific user id they want to find
+    if author_id is not None and author_id != request.user.id:
+        query = ( query ) & Q(author_id=author_id)
+    elif author_id is not None:
+        # author id = same user
+        query = Q(author_id=request.user.id)
 
-    if valid_accept:
-        query = ( Q(visibility=Post.private, author__username=request.user.username) |
-                Q(visibility=Post.public) | Q(visibility=Post.server) |
-                Q(visibility=Post.friend, author__accepter=request.user.id) |
-                Q(visibility=Post.friend, author__requester=request.user.id) |
-                Q(visibility=Post.FOAF, author__requester__requester=request.user.id) |
-                Q(visibility=Post.FOAF, author__requester__accepter=request.user.id) |
-                Q(visibility=Post.FOAF, author__accepter__requester=request.user.id) |
-                Q(visibility=Post.FOAF, author__accepter__accepter=request.user.id) )
+    # Check if user is valid
+    # TODO Decorator?
+    if author_id is not None:
+        try:
+            User.objects.get(id=author_id)
+        except User.DoesNotExist as e:
+            # return a better error for missing user
+            response = JsonResponse({"message":"Author with id %d does not exist" % author_id})
+            response.status_code = 404
+            return response
 
-        # user specified a specific user id they want to find
-        if author_id is not None and author_id != request.user.id:
-            query = ( query ) & Q(author_id=author_id)
-        elif author_id is not None:
-            # author id = same user
-            query = Q(author_id=request.user.id)
+    posts_query = Post.objects.filter(query)
+    posts = list(obj.as_dict() for obj in posts_query)
 
-        # Check if user is valid
-        if author_id is not None:
-            try:
-                User.objects.get(id=author_id)
-            except User.DoesNotExist as e:
-                # return a better error for missing user
-                response = JsonResponse({"message":"Author with id %d does not exist" % author_id})
-                response.status_code = 404
-                return response
+    #TODO Add Pagination
+    data = {"posts":posts};
 
-        posts_query = Post.objects.filter(query)
-        posts = list(obj.as_dict() for obj in posts_query)
-
-        #TODO Add Pagination
-        data = {"posts":posts};
-
-        return JsonResponse(data)
-    else:
-        response = JsonResponse({"message":"Invalid accept type"})
-        response.status_code = 406
-        return response
+    return JsonResponse(data)
 
 def get_post(request,post_id=None,page="0"):
     if post_id is not None:
-        return Post.objects.get(post_id)
+        try:
+            post = Post.objects.get(post_id)
+        except Post.DoesNotExist as e:
+            response = JsonResponse({"message":"Post with id %d does not exist" % post_id})
+            response.status_code = 404
+            return response
+
     else:
-        # TODO: Import these enum visibilities
-        # I don't like hardcoding...
         return Post.objects.filter(visibility=Post.public).order_by('-date')
 
 def friend_request(request,page="1"):
