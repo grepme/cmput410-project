02e4afc64c7ec41a3650a6c95f189601ff07a96e
varsis <chris@varsisstudio.com>
2015-03-11 21:00:52 -0600

    added guid generation for profiles

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 
 
 class Profile(models.Model):
+    def __init__(self, *args, **kwargs):
+        super(Profile, self).__init__(*args, **kwargs)
+        if not self.guid:
+            self.guid = uuid.uuid1().__str__()
+
     author = models.ForeignKey(User)
     display_name = models.CharField(max_length=55)
 
@@ -12,10 +18,10 @@ class Profile(models.Model):
     guid = models.CharField(max_length=55, default=None)
 
     def as_dict(self):
-        return { 
+        return {
         	"id": self.guid,
         	# TODO implement host
-            "host": ""
+            "host": "",
             "displayname" : self.display_name,
             "url": self.host + "/author/" + self.guid
         }
