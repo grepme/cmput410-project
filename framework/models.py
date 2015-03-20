8711837cb865175e740f2b4ae86bcc810154f302
varsis <chris@varsisstudio.com>
2015-03-20 00:25:53 -0600

    database changes for guid

--- framework/models.py
+++ framework/models.py
@@ -8,6 +8,8 @@ import uuid
 class GUIDModel(models.Model):
     guid = models.CharField(primary_key=True, max_length=55)
     def save(self, *args, **kwargs):
-      if not self.guid:
-        self.guid = uuid.uuid1().__str__()
-      super(GUIDModel, self).save(*args, **kwargs)
+        if not self.guid:
+            self.guid = uuid.uuid1().__str__()
+        super(GUIDModel, self).save(*args, **kwargs)
+    class Meta:
+        abstract = True
--- user_profile/models.py
+++ user_profile/models.py
@@ -3,14 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+# using the guid model
+from framework.models import GUIDModel
 
-class Profile(models.Model):
+
+class Profile(GUIDModel):
     author = models.ForeignKey(User)
     display_name = models.CharField(max_length=55)
 
     def as_dict(self):
         return { "id": self.author.id,
-                # Needs to be added "host": 
+                # Needs to be added "host":
                 "displayname" : self.display_name,
-                # "url" : TODO: implement url and host 
+                # "url" : TODO: implement url and host
                 }
