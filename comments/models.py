e95661330298bfd0a852cc142b208a935660732e
Kyle Richelhoff <kyle@mythcomputers.ca>
2015-03-25 16:33:22 -0600

    upload_to added in for FileField

--- comments/models.py
+++ comments/models.py
@@ -11,7 +11,7 @@ class Comment(GUIDModel):
     date = models.DateTimeField()
     # Not sure how to make a "content" field either text or image
     text = models.CharField(max_length=63206, blank=True)
-    image = models.ImageField(blank=True)
+    image = models.ImageField(blank=True, upload_to='comments/%Y/%m/%d')
     post = models.ForeignKey(Post)
     author = models.ForeignKey(Profile)
 
@@ -20,11 +20,11 @@ class Comment(GUIDModel):
 
     def as_dict(self):
         return {
-        	"author": self.author.as_dict(),
-        	"comment": self.text,
-        	"pubDate": self.date,
-        	"guid": self.guid,
+            "author": self.author.as_dict(),
+            "comment": self.text,
+            "pubDate": self.date,
+            "guid": self.guid,
             # not in example-article.json... Do we need these?
             "post": Post.objects.get(post=self.post).as_dict(),
             "image": self.image
-            }
+        }
