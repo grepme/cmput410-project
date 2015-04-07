d89a85878be524e1b60d2f06b0b1e2fc03a7c5ec
varsis <chris@varsisstudio.com>
2015-04-06 21:40:30 -0600

    Profile now using guid

--- user_profile/views.py
+++ user_profile/views.py
@@ -8,21 +8,23 @@ import json
 
 
 @login_required
-def profile(request, author):
+def profile(request, guid):
     """Returns a specific author profile"""
 
     # Get the user's profile. Get is useful for one object.
     # https://docs.djangoproject.com/en/1.7/topics/db/queries/#lookups-that-span-relationships
-    profile = Profile.objects.filter(display_name=author).first()
-    if profile is not None:
+    profile = None
+    try:
+        profile = Profile.objects.get(guid=guid)
+    except Profile.DoesNotExist as e:
         return render(request, "profile/author.html", {'author_profile': profile, 'profile': request.profile})
     # TODO: Return profile not found if that's the case
     return render(request, "profile/author.html", {'author_profile': request.profile, 'profile': request.profile})
 
 @login_required
 def user_profile(request):
-    """This will redirect /profile/ to /profile/<username>"""
-    return redirect(profile, request.user.username)
+    """This will redirect /profile/ to /profile/<profile.guid>"""
+    return redirect(profile, request.profile.guid)
 
 
 @login_required()
@@ -48,7 +50,7 @@ def update_profile(request):
         print("picture:" + str(image))
     else:
         print("no picture")
-        
+
     user.save()
     print (user.author)
     print (user.image)
@@ -69,9 +71,9 @@ def update_profilepic(request):
         print("picture:" + str(image))
     else:
         print("no picture")
-        
+
     user.save()
-    
+
     print (user.image)
 
     return redirect('/profile/')
